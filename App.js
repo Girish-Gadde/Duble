e8325a20b107d3205ac28a860d31946b83e2771c
Prashik Moon <moonprashik17@gmail.com>
2025-01-11 15:26:26 +0530

    commented sm code

--- App.js
+++ App.js
@@ -40,8 +40,8 @@ export default function App() {
   const [mobileNumber, setMobileNumber] = useState(null);
   useEffect(() => {
     const checkMobileNumber = async () => {
-       await AsyncStorage.removeItem("selectedTeamIndex");
-       await AsyncStorage.removeItem("mobileNumber");
+       //await AsyncStorage.removeItem("selectedTeamIndex");
+       //await AsyncStorage.removeItem("mobileNumber");
       try {
         const mobileNo = await AsyncStorage.getItem("mobileNumber");
         // Check if mobileNumber is not null and a string
