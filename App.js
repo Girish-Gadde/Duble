4a7eac122b9d8f121b926c249cfb95bba6623f8f
Prashik17 <moonprashik17@gmail.com>
2025-05-24 13:30:38 +0530

    Accepted by App store test flight

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+
+
 //import Login from '../Login Screens/Login'
 import ProfileDetails from "./components/ProfileScreens/ProfileDetails";
 import { Provider } from "react-redux";
